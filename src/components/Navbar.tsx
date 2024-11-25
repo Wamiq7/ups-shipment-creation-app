d24b5139e08aee413d3b6b881f9b75a8343b3942
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-11-25 22:31:11 +0500

    Fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // const [isToken, setIsToken] = useState("");
 
   // useEffect(() => {
@@ -39,24 +40,32 @@ export default function Navbar() {
   //   }
   // }
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    window.location.reload();
+  };
+
   return (
     <nav className="bg-gray-800 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0 flex items-center">
-              {/* <img
-                className="h-8 w-auto"
-                src="/placeholder.svg?height=32&width=32"
-                alt="Logo"
-              /> */}
               <span className="ml-2 text-xl font-bold text-white">
                 UPS Shipment
               </span>
             </Link>
           </div>
           <div className="flex items-center space-x-4">
-            {localStorage.getItem("token") ? (
+            {isLoggedIn ? (
               <>
                 <Button asChild variant="ghost" className="text-white">
                   <Link href={"/"} aria-current="page">
@@ -71,10 +80,7 @@ export default function Navbar() {
                 <Button
                   variant="default"
                   className="text-gray-800 bg-white hover:bg-gray-200"
-                  onClick={() => {
-                    localStorage.removeItem("token");
-                    window.location.reload();
-                  }}
+                  onClick={handleLogout}
                 >
                   Logout
                 </Button>
